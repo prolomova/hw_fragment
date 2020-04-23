1ce0f9fdbbdc2ec0eb72ae79a67cad54760f3d19
prolomova <prolomova2012@yandex.ru>
2020-04-23 16:29:36 +0500

    hw 7

--- app/src/main/java/com/example/hw_fragment/ui/add/AddNewItemViewModel.kt
+++ app/src/main/java/com/example/hw_fragment/ui/add/AddNewItemViewModel.kt
@@ -3,21 +3,29 @@ package com.example.hw_fragment.ui.add
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.hw_fragment.internal.db.HabitEntity
 import com.example.hw_fragment.internal.HabitApplication
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class AddNewItemViewModel(private val habitIndex: Int?) : ViewModel() {
     val habit: LiveData<HabitEntity?> = HabitApplication.database.habitDao().getById(habitIndex)
 
     fun save(habit: HabitEntity) {
-        val habitDao = HabitApplication.database.habitDao()
-        Log.d("save habit", habitIndex.toString())
-        if (habitIndex == null) {
-            habitDao.insert(habit) }
-        else {
-            habit.id = habitIndex
-            habitDao.update(habit)
+        viewModelScope.launch(Dispatchers.Default) {
+            val habitDao = HabitApplication.database.habitDao()
+            Log.d("save habit", habitIndex.toString())
+            if (habitIndex == null) {
+                withContext(Dispatchers.IO) { habitDao.insert(habit) }
+            }
+            else {
+                habit.id = habitIndex
+                withContext(Dispatchers.IO) { habitDao.update(habit) }
+            }
         }
+
     }
 }
